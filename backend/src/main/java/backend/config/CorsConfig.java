095cc507349e4601294e7c9f025ae393670d6fb0
vienoz <niclas.arb@outlook.com>
2023-03-15 00:04:16 +0100

    added javadoc and cors headers

--- backend/src/main/java/backend/config/CorsConfig.java
+++ backend/src/main/java/backend/config/CorsConfig.java
@@ -6,10 +6,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-
+/**
+ * Class used for configuring cors settings
+ */
 @Configuration
 public class CorsConfig {
 
+    /**
+     * sets cors header to allow all origins
+     *
+     * @return CorsFilter with set header options
+     */
     @Bean
     public CorsFilter corsFilter(){
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
